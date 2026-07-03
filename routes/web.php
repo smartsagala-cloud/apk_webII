6eab4b33b902b36b8109216deb81252d14a7ee75
Smart Sagala <smartsagala@gmail.com>
2026-07-03 11:54:15 +0700

    Pertemuan 2 Route dan Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetingsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GreetingsController::class, 'welcome']);
+
+Route::get('/halo/{name}/{npm}', [GreetingsController::class, 'greet']);
